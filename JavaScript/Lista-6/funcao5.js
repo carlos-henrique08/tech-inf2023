32212ccfcee1c9f195e92a002c204e91424298e4
Carlos <carloshenriqueds2004@gmail.com>
2024-09-11 09:05:45 -0300

    ...

--- JavaScript/Lista-6/funcao5.js
+++ JavaScript/Lista-6/funcao5.js
@@ -1,14 +1,12 @@
-function rowToJSON(Header, content){
-const obj = {};
-for (let i=0; i<Header.lenght; i++){
-    let key = Header[i];
-    let value = content[i];
-
-    obj[key] = value;
-
-
+function rowToJSON(Header, row) {
+ let obj = {};
+for(let i=0; i < Header.length; i++) {
+    obj[Header[i]]= row[i];
 }
 return obj;
 }
 
 console.log(rowToJSON(["nome", "idade"],["João", "28"]));
+console.log(rowToJSON( ["Filme", "Diretor", "Ano", "Duração", "Gênero", "País de Origem",
+    "Receita Bruta"],["Parasita", "Bong Joon-ho", "2019", "132", "Drama", "Coréia do Sul",
+    "258.8M"]));

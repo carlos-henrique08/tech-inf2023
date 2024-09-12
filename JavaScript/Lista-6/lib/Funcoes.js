689355f0c22b9140d9b876300e5dcd03f04a315c
Carlos <carloshenriqueds2004@gmail.com>
2024-09-12 11:52:30 -0300

    ..

--- JavaScript/Lista-6/lib/Funcoes.js
+++ JavaScript/Lista-6/lib/Funcoes.js
@@ -1,4 +1,4 @@
-/**
+/**--FUNÇAO - 1--
  * - Essa função converte os dados em CSV onde cada objeto Representa uma linha.
  * @param {line} csvtext - Uma string contendo o conteúdo de um CSV.
  * @returns - Uma lista de strings, onde cada string representa uma linha do CSV.
@@ -7,7 +7,7 @@ function csvTolines(csvtext){
 return csvtext.split('\n'); 
 }
 
-/**
+/**--FUNÇAO - 2--
  * - Essa função separa os cabeçalho da linha dos dados.
  * @param {lines} Lines - Uma lista de strings, onde cada string representa uma linha do CSV.
  * @returns {row} - Uma lista de listas, onde cada lista interna contém as colunas de uma linha do CSV.
@@ -21,7 +21,7 @@ Columns.push(Lines[i].split(';'));
 return Columns;
 }
 
-/**
+/**--FUNÇAO - 3--
  * - Essa função extrai o cabeçalho e mostra.
  * @param {rows} Header - Uma lista de listas de strings, onde cada lista interna representa colunas de uma linha
 do CSV.
@@ -31,7 +31,7 @@ function  extractHeader (Header) {
 return Header[0];
 }
 
-/**
+/**--FUNÇAO - 4--
  *  - Essa função extrai as informações e mostra somente os dados e não mostra o cabeçalho.
  * @param {content} columns - Uma lista de listas de strings, onde cada lista interna representa colunas de uma linha
 do CSV.
@@ -42,7 +42,7 @@ function  extractContent(columns) {
 }
 
 
-/**
+/**--FUNÇAO - 5--
  *  - Essa função organizar as informações em seus respectivos parametro.
  * @param {cabeçalho} Header - Duas listas de mesmo tamanho: A primeira lista representa o cabeçalho da tabela.
  * @param {*} row - A segunda lista representa uma única linha do conteúdo da tabela. 
@@ -57,7 +57,7 @@ obj[Header[i]]= row[i];
 return obj;
 }
 
-/**
+/**--FUNÇAO - 6--
  * 
  * @param {key} header - Uma lista de strings representando o cabeçalho
  * @param {value} content - uma lista de listas representando o conteúdo do CSV
--- JavaScript/Lista-6/lib/printCSV.js
+++ JavaScript/Lista-6/lib/printCSV.js
@@ -1,4 +1,13 @@
-const Funcoes = require("./");
+const csvtools = require('../lib/Funcoes');
 
-function printCSV(Funcoes, columns){
-}
+function printCSV(csvtext , columns){
+    const lines = csvtools.csvTolines(csvtext);
+    const rows = csvtools.linesToColumns(lines);
+    const Header = csvtools.extractHeader(rows);
+    const content = csvtools.extractContent(rows);
+    const json = csvtools.columnsToJSON(Header);
+
+    console.table(json, columns);
+} 
+
+module.exports = (printCSV);
